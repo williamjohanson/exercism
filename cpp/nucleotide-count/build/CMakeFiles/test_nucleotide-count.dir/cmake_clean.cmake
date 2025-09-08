eea1836901695a49d4133ec829665423b59b4fb9
William Johanson <william.j@automationelec.com>
2025-09-08 20:40:28 +1000

    Why had all this ended up there.

--- cpp/nucleotide-count/build/CMakeFiles/test_nucleotide-count.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/test_nucleotide-count"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/test_nucleotide-count.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
